37d248e54aec20fdf3a7ed48bfbc5a1243c0737c
Drool3008 <dhawalpawanarkar123@gmail.com>
2023-10-25 19:25:47 +0530

    -

--- ClassesChallenge/src/Main.java
+++ ClassesChallenge/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Class c = new Class();
+        c.setAccountBalance(5000);
+        c.setAccountNumber(11910389);
+        c.setCustomerEmail("drool1010@gmail.com");
+        c.setCustomerName("dhawal");
+        c.setPhoneNumber("9876543210");
+        c.display();
+        c.deposit();
+//        c.display();
     }
 }
